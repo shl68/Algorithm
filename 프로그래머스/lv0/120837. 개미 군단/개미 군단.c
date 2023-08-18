af4b56c60ee2a62dcf15440eee8d1f142e3b5fad
shl68 <105649474+shl68@users.noreply.github.com>
2023-08-18 11:15:48 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int hp) {
+    int answer = 0;
+    int cap = 5;
+    int sol = 3;
+    int il = 1;
+    
+    
+    
+    
+    answer = ( hp / 5) + (hp % 5 / 3) + (hp % 5 % 3 / 1); 
+        
+        
+        
+        
+    return answer;
+    
+    
+    
+}
