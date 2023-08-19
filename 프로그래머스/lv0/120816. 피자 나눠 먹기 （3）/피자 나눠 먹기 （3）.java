9694f71a72881b9481b953f18b53e0cc58211bf6
shl68 <105649474+shl68@users.noreply.github.com>
2023-08-19 12:07:48 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        if (n % slice == 0)
+        {
+            answer = n / slice;
+        }
+        else if (n % slice > 0)
+        {
+            answer = n / slice + 1;
+        }
+        return answer;
+    }
+}
